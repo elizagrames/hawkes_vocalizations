a47900f2efaae516f6c9b73a68f9761a8c3df080
Eliza Grames <eliza.grames@uconn.edu>
2019-11-29 14:00:29 -0500

    forgot to index mu by i, oops

--- scripts/JAGS_model.R
+++ scripts/JAGS_model.R
@@ -1,10 +1,11 @@
 model{
   # Hawkes self-exciting point process model
   for (i in 1:nsites) {
+    mu[i] ~ dnorm(mu.mu, mu.tau)T(0.0001, 2)
     for (j in 1:nobs) {
       t[i, j] ~ dpois(lambda[i, j])
       
-      lambda[i, j] = mu + gamma[i, j]
+      lambda[i, j] = mu[i] + gamma[i, j]
       
       
       for (n in 1:maxmemory) {
@@ -18,7 +19,8 @@ model{
   }
   
   # Priors
-  mu ~ dgamma(0.0001, 0.0001)T(0.0001, 1)
+  mu.mu ~ dgamma(0.0001, 0.0001)
+  mu.tau ~ dgamma(0.0001, 0.0001)
   
   alpha ~ dgamma(0.0001, 0.0001)T(0.0001, 1)
   
@@ -35,13 +37,15 @@ model{
   
   
     for (i in 1:nsites) {
-      lambda2[i] ~ dgamma(0.0001, 0.0001)T(0.0001, 2)
+      lambda2[i] ~ dnorm(lambda2.mu, lambda2.tau)T(0.0001, 2)
       for (j in 1:nobs) {
         t2[i, j] ~ dpois(lambda2[i])
       }
   
     
-  }
+    }
+  lambda2.mu ~ dgamma(0.0001, 0.0001)
+  lambda2.tau ~ dgamma(0.0001, 0.0001)
   # Regular Poisson model
   
   # simulation of poisson

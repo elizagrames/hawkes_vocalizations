1450883480d385419f7c08752c082ca506b568c5
Eliza Grames <eliza.grames@uconn.edu>
2019-12-01 11:13:24 -0500

    updated code for seeing if models converged and started code for basic analysis

--- scripts/checking_fit.R
+++ scripts/checking_fit.R
@@ -1,16 +1,56 @@
 files <- paste("./output/", list.files("./output/"), sep="")
+i <- 0
 
 i <- i+1
 load(files[i])
 params <- site_model$BUGSoutput$summary
 round(head(params, 20),4)
-hist(params[,8])
+type <- substr(rownames(params), 1, 5)
+only_these <- which(type!="sim_t")
+hist(params[only_these,8], main=files[i])
 MCMCvis::MCMCtrace(site_model, params=c("alpha", "beta", "mu"), pdf=FALSE)
 
-#bad models
-#beaver brook large
-#rockville md is really bad
-#rockville small is questionable beta and alpha spike to .4 sometimes but rhat okay
-#something went wrong with rockville small gamma[1,everything]
-#nye holman md is a bit wonky, but rhat okay
+params[which(params[only_these,8]>1.1),8]
+dev.off()
+
+
+## BAD MODELS
+
+ci_counts <- function(hawk, pois, t){
+  nsims <- dim(hawk)[1]
+  nsites <- dim(hawk)[2]
+  
+  tmp.h <- tmp.p <- array(dim=c(nsims, nsites))
+  for(s in 1:nsims){
+    for(i in 1:nsites){
+      tmp.h[s,i] <- length(which(hawk[s,i,]>0))
+      tmp.p[s,i] <- length(which(pois[s,i,]>0))
+    }
+  }
+  
+  hestimates <- apply(tmp.h, 2, mean)
+  hest.ci <- apply(tmp.h, 2, quantile, c(0.025, 0.975))
+  pestimates <- apply(tmp.p, 2, mean)
+  pest.ci <- apply(tmp.p, 2, quantile, c(0.025, 0.975))
+  counts <- rowSums(t)
+  tbl <- round(cbind(counts, hestimates, pestimates),2)
+  colnames(tbl) <- c("obs", "hawkes", "poisson")
+  return(tbl)
+}
+
+bad_files <- c()
+sites <- sites[-bad_files]
+files <- paste("./output/", list.files("./output/"), sep="")
+files <- files[-bad_files]
+i <- 0
+
+i <- i+1
+load(files[i])
+
+hawk <- site_model$BUGSoutput$sims.list$sim_t
+pois <- site_model$BUGSoutput$sims.list$sim_t2
+t <- t(all_events[, which(colnames(all_events) == sites[i])])
+
+table_1 <- ci_counts(hawk, pois, t)
+
 

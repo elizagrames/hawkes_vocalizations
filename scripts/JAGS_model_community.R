07a9a52cd1d3d58a133ed296aa12c53a64a1a08d
Eliza Grames <eliza.grames@uconn.edu>
2019-12-02 08:03:06 -0500

    set hyperparameters because of pseudorep

--- scripts/JAGS_model_community.R
+++ scripts/JAGS_model_community.R
@@ -1,7 +1,7 @@
 model{
   # Hawkes self-exciting point process model
   for (i in 1:nsites) {
-    mu[i] ~ dgamma(0.0001, 0.0001)T(0.0001, 2)
+    mu[i] ~ dgamma(mu.mu, mu.tau)T(0.0001, 2)
     for (j in 1:nobs) {
       t[i, j] ~ dpois(lambda[i, j])
       
@@ -24,6 +24,8 @@ model{
   
   beta ~ dgamma(0.0001, 0.0001)T(0.0001, 5)
   
+  mu.mu ~ dgamma(0.0001, 0.0001)T(0.0001, 2)
+  tau.mu ~ dgamma(0.0001, 0.0001)T(0.0001, 10)
   # simulation hawkes
   
     for(i in 1:nsites){
@@ -38,9 +40,12 @@ model{
       for (j in 1:nobs) {
         t2[i, j] ~ dpois(lambda2[i])
       }
-      lambda2[i] ~ dgamma(0.0001, 0.0001)T(0.0001, 2)
+      lambda2[i] ~ dgamma(mu.lambda2, tau.lambda2)T(0.0001, 2)
     
     }
+  
+  mu.lambda2 ~ dgamma(0.0001, 0.0001)T(0.0001, 2)
+  tau.lambda2 ~ dgamma(0.0001, 0.0001)T(0.0001, 10)
   # Regular Poisson model
   
   # simulation of poisson

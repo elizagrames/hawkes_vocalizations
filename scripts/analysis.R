84aea3dbda42c2590fea319b6964118e2b5ba526
Eliza Grames <eliza.grames@uconn.edu>
2019-12-30 17:59:58 -0500

    setting up script for checking fit on the cluster

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -1,45 +1,20 @@
 #### -1: Functions ####
-ci_counts <- function(hawk, pois, t){
-  nsims <- dim(hawk)[1]
-  nsites <- dim(hawk)[2]
+ci_counts <- function(mod, truth){
+  nsims <- dim(mod)[1]
+  nsites <- dim(mod)[2]
   
-  tmp.h <- tmp.p <- array(dim=c(nsims, nsites))
+  tmp.mod <- array(dim=c(nsims, nsites))
   for(s in 1:nsims){
     for(i in 1:nsites){
-      tmp.h[s,i] <- length(which(hawk[s,i,]>0)) - sum(t[i,])
-      tmp.p[s,i] <- length(which(pois[s,i,]>0)) - sum(t[i,])
+      tmp.mod[s,i] <- length(which(mod[s,i,]>0)) - sum(truth[i,])
     }
   }
   
-  hestimates <- apply(tmp.h, 2, mean)
-  hest.ci <- apply(tmp.h, 2, quantile, c(0.025, 0.975))
-  pestimates <- apply(tmp.p, 2, mean)
-  pest.ci <- apply(tmp.p, 2, quantile, c(0.025, 0.975))
-  counts <- rowSums(t)
-  tbl <- round(cbind(counts, hest.ci[1,], hestimates, hest.ci[2,], pest.ci[1,], pestimates, pest.ci[2,]),2)
-  colnames(tbl) <- c("obs", "h.low", "hawkes", "h.up", "p.low", "poisson", "p.up")
-  return(tbl)
-}
-
-ci_counts2 <- function(hawk, pois, t){
-  nsims <- dim(hawk)[1]
-  nsites <- dim(hawk)[2]
-  
-  tmp.h <- tmp.p <- array(dim=c(nsims, nsites))
-  for(s in 1:nsims){
-    for(i in 1:nsites){
-      tmp.h[s,i] <- (length(which(hawk[s,i,]>0)) - sum(t[i,]))/sum(t[i,])
-      tmp.p[s,i] <- (length(which(pois[s,i,]>0)) - sum(t[i,]))/sum(t[i,])
-    }
-  }
-  
-  hestimates <- apply(tmp.h, 2, mean)
-  hest.ci <- apply(tmp.h, 2, quantile, c(0.025, 0.975))
-  pestimates <- apply(tmp.p, 2, mean)
-  pest.ci <- apply(tmp.p, 2, quantile, c(0.025, 0.975))
-  counts <- rowSums(t)
-  tbl <- round(cbind(counts, hest.ci[1,], hestimates, hest.ci[2,], pest.ci[1,], pestimates, pest.ci[2,]),2)
-  colnames(tbl) <- c("obs", "h.low", "hawkes", "h.up", "p.low", "poisson", "p.up")
+  mod.mean <- apply(tmp.mod, 2, mean)
+  mod.ci <- apply(tmp.mod, 2, quantile, c(0.025, 0.975))
+  counts <- rowSums(truth)
+  tbl <- cbind(counts, mod.ci[1,], mod.mean, mod.ci[2,])
+  colnames(tbl) <- c("obs", "li", "x", "ui")
   return(tbl)
 }
 
@@ -47,62 +22,42 @@ mcc <- function(tp, tn, fp, fn){
   ((tp*tn) - (fp*fn))/(sqrt(tp+fp)*sqrt(tp+fn)*sqrt(tn+fp)*sqrt(tn+fn))
 }
 
-calculate_props <- function(hawk, pois, t){
-  nsims <- dim(hawk)[1]
-  nsites <- dim(hawk)[2]
-  nobs <- dim(hawk)[3]
+calculate_props <- function(mod, truth, size){
+  nsims <- dim(mod)[1]
+  nsites <- dim(mod)[2]
+  nobs <- dim(mod)[3]
   
-  tmp.h <- tmp.p <- diffph <- array(dim=c(nsims, nsites))
+  tmp <- array(dim=c(nsims, nsites))
   for(s in 1:nsims){
     for(i in 1:nsites){
-      obs.e <- which(t[i,]>0)
+      obs.e <- which(truth[i,]>0)
       window <- obs.e
       for(m in 1:length(obs.e)){
-        window <- append(window, obs.e[m]+1)
-        window <- append(window, obs.e[m]-1)
+        window <- append(window, obs.e[m]+size)
+        window <- append(window, obs.e[m]-size)
       }
-      hawk.pos <- which(hawk[s,i,]>0)
-      hawk.neg <- which(hawk[s,i,]==0)
-      
-      pois.pos <- which(pois[s,i,]>0)
-      pois.neg <- which(hawk[s,i,]==0)
+      mod.pos <- which(mod[s,i,]>0)
+      mod.neg <- which(mod[s,i,]==0)
       
       obs.pos <- obs.e
-      obs.neg <- which(t[i,]==0)
+      obs.neg <- which(truth[i,]==0)
       
-      tp.h <- sum(hawk.pos %in% window) 
-      tn.h <- sum(hawk.neg %in% obs.neg)
-      fp.h <- sum((hawk.pos %in% window)!=TRUE)
-      fn.h <- sum(hawk.neg %in% obs.pos)
+      tp <- sum(hawk.pos %in% window) 
+      tn <- sum(hawk.neg %in% obs.neg)
+      fp <- sum((hawk.pos %in% window)!=TRUE)
+      fn <- sum(hawk.neg %in% obs.pos)
       
-      tp.p <- sum(pois.pos %in% window) 
-      tn.p <- sum(pois.neg %in% obs.neg)
-      fp.p <- sum((pois.pos %in% window)!=TRUE)
-      fn.p <- sum(pois.neg %in% obs.pos)
-      
-      tmp.h[s,i] <- mcc(tp.h, tn.h, fp.h, fn.h)
-      tmp.p[s,i] <- mcc(tp.p, tn.p, fp.p, fn.p)
-      diffph[s,i] <- tmp.h[s,i] - tmp.p[s,i]
-      
+      tmp[s,i] <- mcc(tp, tn, fp, fn)
     }
   }
-  tmp.h[is.na(tmp.h)] <- NA
-  tmp.p[is.na(tmp.p)] <- NA
+  tmp[is.na(tmp)] <- NA
+
+  mod.mean <- apply(tmp.mod, 2, mean)
+  mod.ci <- apply(tmp.mod, 2, quantile, c(0.025, 0.975))
+  counts <- rowSums(truth)
+  tbl <- cbind(mod.ci[1,], mod.mean, mod.ci[2,])
+  colnames(tbl) <- c("li", "x", "ui")
   
-  hestimates <- apply(tmp.h, 2, mean, na.rm=TRUE)
-  
-  hest.ci <- apply(tmp.h, 2, quantile, c(0.025, 0.975), na.rm=TRUE)
-  pestimates <- apply(tmp.p, 2, mean, na.rm=TRUE)
-  pest.ci <- apply(tmp.p, 2, quantile, c(0.025, 0.975), na.rm=TRUE)
-  destimates <- apply(diffph, 2, mean, na.rm=TRUE)
-  dest.ci <- apply(diffph, 2, quantile, c(0.025, 0.975), na.rm=TRUE)
-  
-  tbl <- cbind(hest.ci[1,], hestimates, hest.ci[2,], 
-               pest.ci[1,], pestimates, pest.ci[2,],
-               dest.ci[1,], destimates, dest.ci[2,])
-  colnames(tbl) <- c("h.low", "hawkes", "h.up", 
-                     "p.low", "poisson", "p.up",
-                     "d.low", "diff", "d.up")
   return(tbl)
 }
 
@@ -134,53 +89,66 @@ if(mu==TRUE){
   gestimates <- apply(tmp, 2, mean)
   gest.ci <- apply(tmp, 2, quantile, c(0.025, 0.975), na.rm=TRUE)
   tbl <- as.data.frame(cbind(gest.ci[1,], gestimates, gest.ci[2,]))
-  colnames(tbl) <- c("low", "mean", "up")
+  colnames(tbl) <- c("li", "x", "ui")
   return(tbl)
 }
 
 #### 0: Load in the data ####
 
+models <- list.files("./output/community/")
+filenames <- paste("./output/community/", models, sep="")
+filenames <- filenames[which(stringr::str_detect(filenames, "RData"))]
+#filenames <- filenames[-13]
 
-files <- paste("./output/community/", list.files("./output/community/"), sep="")
-bad_files <- c(1, 3, 18)
-files <- files[-bad_files]
-sites <- sites[-1] # oops i forgot to run babcock
-sites <- sites[-bad_files]
-condits <- events <- list(); length(condits) <- length(events) <- length(sites)
-events2 <- events
-
-for(i in 1:length(sites)){
-  
-load(files[i])
-hawk <- site_model$BUGSoutput$sims.list$sim_t
-pois <- site_model$BUGSoutput$sims.list$sim_t2
-gamma <- site_model$BUGSoutput$sims.list$gamma
-lambda <- site_model$BUGSoutput$sims.list$lambda
-mu <- site_model$BUGSoutput$sims.list$mu
-t <- t(all_events[, which(colnames(all_events) == sites[i])])
-
-# Drop any observations where there is no conditional intensity
-# why? because then we are just comparing two poisson processes, which is silly
-gamma_means <- apply(gamma, 2, mean)
-if(any(gamma_means==0)){
-  hawk <- hawk[,-which(gamma_means==0),]
-  pois <- pois[,-which(gamma_means==0),]
-  t <- t[-which(gamma_means==0),]
-  gamma <- gamma[,-which(gamma_means==0),]
-  lambda <- lambda[,-which(gamma_means==0),]
-  mu <- mu[,-which(gamma_means==0)]
+for(i in 1:length(models)){
+  if(i==1){sites <- c()}
+  sites[i] <- strsplit(models[i], "\\.")[[1]][1]
 }
 
-if(sum(gamma_means>0)>1){
-  
+params <- c("mu",
+            "alpha", 
+            "beta", 
+            "lambdaM",
+            "lambdaP",
+            "muW",
+            "gammaW")
+
+condits <- events <- list(); length(condits) <- length(events) <- length(sites)
+all_events <- read.csv("all_events.csv")
+
+for(i in 1:length(filenames)){
+  load(filenames[i])
+  estimates <- site_model$BUGSoutput$summary
+  types <- strsplit(rownames(estimates, "\\["))[[1]][1]
+  rhats <- estimates[types %in% params, 'Rhat']
+  if(any(rhats>1.1)){
+    tag <- "bad"
+  }else{
+    tag <- "good"
+  }
+  write.csv(estimates, file=paste("./", sites[i], tag, ".csv", sep=""))
+
+    if(tag=="good"){
+    truth <- t(all_events[, which(colnames(all_events) == sites[i])])
+    eventsH <- ci_counts(site_model$BUGSoutput$sims.list$sim_tH, truth)
+    eventsM <- ci_counts(site_model$BUGSoutput$sims.list$sim_tM, truth)
+    eventsP <- ci_counts(site_model$BUGSoutput$sims.list$sim_tP, truth)
+    eventsW <- ci_counts(site_model$BUGSoutput$sims.list$sim_tW, truth)
+    conditsH[[i]] <- ci_condit(param=site_model$BUGSoutput$sims.list$gamma, lambdaH, mu=FALSE)
+    conditsW[[i]] <- ci_condit(param=site_model$BUGSoutput$sims.list$gammaW, lambdaW, mu=FALSE)
+    
+    output <- list(eventsH, eventsM, eventsP, eventsW, conditsH, conditsW)
+    save(output, file=paste("./", sites[i], "output_for_plots.RData", sep=""))
+    rm(truth, eventsH, eventsM, eventsP, eventsW, conditsH, conditsW)
+    }
+}
+
+
 #### 1: Comparison of total number of estimated events ####
 
 # are they both able to estimate observed values well?
 # need to load in each extract mean + ci of total events for each iteration
 # compare hawkes to poisson to observed values
-
-events[[i]] <- ci_counts(hawk, pois, t)
-events2[[i]] <- ci_counts2(hawk, pois, t)
 
 ##### 2: Comparison of correspondance ####
 
@@ -204,61 +172,4 @@ events2[[i]] <- ci_counts2(hawk, pois, t)
 
 # look at the relative contribution of mu and gamma within the Hawkes models
 
-condits[[i]] <- ci_condit(param=gamma, lambda, mu=FALSE)}
-}
-
-##### Plots ####
-col1 <- RColorBrewer::brewer.pal(11,"Spectral")[1]
-col2 <- RColorBrewer::brewer.pal(11,"Spectral")[10]
-
-#evsave <- events; evsav2 <- events2
-
-events <- events[-13]
-events2 <- events2[-13]
-
-for(i in 1:length(events)){
-  if(i == 1){
-    all_calls <- events[[i]]
-  }
-  all_calls <- rbind(all_calls, events[[i]])
-}
-
-table_1 <- as.data.frame(all_calls)
-table_1 <- table_1[-which(table_1$obs<7),]
-rownames(table_1) <- seq(1, nrow(table_1))
-means <- apply(table_1, 2, mean)
-vars <- apply(table_1, 2, var)
-
-
-midpoints <- as.numeric(rbind(table_1$hawkes, table_1$poisson, rep(NA, nrow(table_1))))
-li <- as.numeric(rbind(table_1$h.low, table_1$p.low, rep(NA, nrow(table_1))))
-ui <- as.numeric(rbind(table_1$h.up, table_1$p.up, rep(NA, nrow(table_1))))
-plot(midpoints, col=c(col1, col2, "white"), pch=c(16,15,1), cex=1, las=1, 
-     xlim=c(0,nrow(table_1)*3),
-     ylim=c(min(li[!is.na(li)]), max(ui[!is.na(ui)])))
-abline(h=0, lty=2, lwd=2)
-arrows(x0 = seq(1, nrow(table_1)*3,1), y0 = li, x1 = seq(1, nrow(table_1)*3,1), y1 = ui,
-       col=c(col1, col2, "white"), angle=90, length=.03, code = 3, lwd=1)
-
-condits <- condits[-13]
-for(i in 1:length(condits)){
-  if(i == 1){
-    all_gams <- condits[[i]]
-  }
-  all_gams <- rbind(all_gams, condits[[i]])
-}
-
-table_1 <- as.data.frame(all_gams)
-table_1 <- rbind(table_1, apply(table_1, 2, mean))
-sitecols <- rownames(table_1)
-
-midpoints <- as.numeric(rbind(table_1$mean, rep(NA, nrow(table_1))))
-li <- as.numeric(rbind(table_1$low, rep(NA, nrow(table_1))))
-ui <- as.numeric(rbind(table_1$up, rep(NA, nrow(table_1))))
-plot(midpoints, col=c(col1, "white"), pch=c(16,1), cex=1, las=1, 
-     xlim=c(0,nrow(table_1)*2),
-     ylim=c(min(li[!is.na(li)]), max(ui[!is.na(ui)])))
-abline(h=0, lty=2, lwd=2)
-arrows(x0 = seq(1, nrow(table_1)*2,1), y0 = li, x1 = seq(1, nrow(table_1)*2,1), y1 = ui,
-       col=c(col1, "white"), angle=90, length=.03, code = 3, lwd=1)
 
